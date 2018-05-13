c91e5655d52772aa42f9977e83797cd41dd81bbe
Eric Laursen <ericjlaursen@gmail.com>
2018-05-13 14:48:12 -0400

    exports schema.

--- models/user.js
+++ models/user.js
@@ -8,4 +8,6 @@ const UserSchema = mongoose.Schema({
 	email: {type: String, required: true },
 	username: {type: String, required: true },
 	password: {type: String, required: true }
-})
+});
+
+const User = module.exports = mongoose.model('User', UserSchema);
