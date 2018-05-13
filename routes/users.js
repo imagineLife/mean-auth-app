84380bcfc658a1e6a4b253bc44024b74df27f431
Eric Laursen <ericjlaursen@gmail.com>
2018-05-13 14:42:41 -0400

    Removes validate && sets register to post.

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 //Register endpoint
-router.get('/register', (req, res, next) => {
+router.post('/register', (req, res, next) => {
 	res.send('User Register endpoint served!');
 })
 
@@ -14,11 +14,6 @@ router.get('/authenticate', (req, res, next) => {
 //Profiel endpoint, protecting later
 router.get('/profile', (req, res, next) => {
 	res.send('User profile endpoint served!');
-})
-
-//Validate endpoint, checking if token matches
-router.get('/validate', (req, res, next) => {
-	res.send('User validation endpoint served!');
 })
 
 module.exports = router;
