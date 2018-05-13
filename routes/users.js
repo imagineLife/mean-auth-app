66a9d52facc92d7f13d601f81f212237ee768906
Eric Laursen <ericjlaursen@gmail.com>
2018-05-13 13:56:26 -0400

    3 more endpoint placeholders.

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,19 @@ router.get('/register', (req, res, next) => {
 	res.send('User Register endpoint served!');
 })
 
+//Authenticate endpoint
+router.get('/authenticate', (req, res, next) => {
+	res.send('User Auth endpoint served!');
+})
+
+//Profiel endpoint, protecting later
+router.get('/profile', (req, res, next) => {
+	res.send('User profile endpoint served!');
+})
+
+//Validate endpoint, checking if token matches
+router.get('/validate', (req, res, next) => {
+	res.send('User validation endpoint served!');
+})
+
 module.exports = router;
