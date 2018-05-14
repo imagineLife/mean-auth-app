5c944ad8673cda1396ff60a520aea9241f1f1427
Eric Laursen <ericjlaursen@gmail.com>
2018-05-14 07:47:29 -0400

    Includes jwt & user model.

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+const User = require('../model/user');
 
 //Register endpoint
 router.post('/register', (req, res, next) => {
