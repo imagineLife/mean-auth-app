209756e38a54519f51b2d9a3f3def2d8cd6add52
Eric Laursen <ericjlaursen@gmail.com>
2018-05-14 12:45:44 -0400

    Includes email in new User, includes err output.

--- routes/users.js
+++ routes/users.js
@@ -2,22 +2,23 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 const jwt = require('jsonwebtoken');
-const User = require('../model/user');
+const User = require('../models/user');
 
 //Register endpoint
 router.post('/register', (req, res, next) => {
 	
 	//new User object
 	let newUser = new User({
-		name: body.name,
-		username: body.username,
-		password: body.password
+		name: req.body.name,
+		username: req.body.username,
+		email: req.body.email,
+		password: req.body.password,
 	})
 
 	//add the new user obj to mongo
 	User.addUser(newUser, (err, user) => {
 		if(err){
-			res.json({success:false, msg: 'Failed to register user'})
+			res.json({success:false, msg: 'Failed to register user', err: err})
 		}else{
 			res.json({success: true, msg: 'User successfulle registered!'})
 		}
