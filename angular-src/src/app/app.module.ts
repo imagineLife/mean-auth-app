7bf3d829295148e7e6aa6e17baf05dac13cc4803
Eric Laursen <ericjlaursen@gmail.com>
2018-05-16 16:18:55 -0400

    Imports validate service.

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -12,6 +12,7 @@ import { HomeComponent } from './components/home/home.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ProfileComponent } from './components/profile/profile.component';
 
+import {ValidateService} from './services/validate.service';
 const appRoutes: Routes = [
   {
     path: '',
