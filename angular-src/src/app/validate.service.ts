79d6f49840d667fc1fdf598c2b9105278ca8f503
Eric Laursen <ericjlaursen@gmail.com>
2018-05-16 16:40:07 -0400

    Includes regexp validateEmail fn.

--- angular-src/src/app/validate.service.ts
+++ angular-src/src/app/validate.service.ts
@@ -13,4 +13,8 @@ export class ValidateService {
   	}
   }
 
+  validateEmail(emailAddr){
+  	const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  	return re.test(emailAddr);
+  }
 }
